a0e8a77036c7ae07fd0e943901ee6da3fe804551
MiniDigger <admin@minidigger.me>
2016-12-12 20:20:14 +0100

    lets give up for now

--- api/src/main/java/me/minidigger/voxelgameslib/api/log/LoggerTest.java
+++ api/src/main/java/me/minidigger/voxelgameslib/api/log/LoggerTest.java
@@ -1,15 +1,19 @@
 package me.minidigger.voxelgameslib.api.log;
 
+import lombok.Getter;
 import me.minidigger.voxelgameslib.annotations.log.LoggerInfo;
 
 /**
  * Created by Martin on 10.12.2016.
  */
+@Getter
 @LoggerInfo(test = "test")
 public class LoggerTest {
 
+    private String test;
+
     public LoggerTest() {
         System.out.println("logger test");
-        System.out.println("logger = " + log);
+//        System.out.println("logger = " + log + getTest());
     }
 }
