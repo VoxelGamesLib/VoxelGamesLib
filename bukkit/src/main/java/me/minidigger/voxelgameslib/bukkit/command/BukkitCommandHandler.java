4048e9efe19dd40e3372f66f6b12139a7545edaa
MiniDigger <admin@minidigger.me>
2016-12-20 17:57:14 +0100

    fix getting of the knownCommands map

--- bukkit/src/main/java/me/minidigger/voxelgameslib/bukkit/command/BukkitCommandHandler.java
+++ bukkit/src/main/java/me/minidigger/voxelgameslib/bukkit/command/BukkitCommandHandler.java
@@ -1,21 +1,24 @@
 package me.minidigger.voxelgameslib.bukkit.command;
 
+import org.bukkit.command.CommandMap;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.SimpleCommandMap;
+import org.bukkit.command.defaults.BukkitCommand;
+import org.bukkit.plugin.SimplePluginManager;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Map;
+
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import lombok.extern.java.Log;
 import me.minidigger.voxelgameslib.api.command.CommandHandler;
 import me.minidigger.voxelgameslib.api.command.CommandInfo;
 import me.minidigger.voxelgameslib.bukkit.VoxelGamesLibBukkit;
-
-import org.bukkit.command.CommandMap;
-import org.bukkit.command.SimpleCommandMap;
-import org.bukkit.plugin.SimplePluginManager;
-
-import lombok.extern.java.Log;
 
 @Log
 @Singleton
@@ -48,8 +51,18 @@ public class BukkitCommandHandler extends CommandHandler {
                 Field knownCommandField = SimpleCommandMap.class.getDeclaredField("knownCommands");
                 knownCommandField.setAccessible(true);
                 Object obj = knownCommandField.get(bukkitCommandMap);
-                if (obj.getClass().isAssignableFrom(Map.class)) {
+                System.out.println(obj.getClass().getName());
+                if (obj.getClass().isAssignableFrom(HashMap.class)) {
                     knownCommands = (Map<String, org.bukkit.command.Command>) obj;
+
+                    // DEBUG
+                    knownCommands.put("testcommandtest", new BukkitCommand("testcommandtest") {
+                        @Override
+                        public boolean execute(CommandSender commandSender, String s, String[] strings) {
+                            commandSender.sendMessage("testcommandtest");
+                            return true;
+                        }
+                    });
                 } else {
                     log.severe("Could not get knownCommands from the SimpleCommandMap (returned unexpected object), can't unregister any commands!");
                 }
@@ -67,7 +80,7 @@ public class BukkitCommandHandler extends CommandHandler {
     @Override
     protected void registerCommand(@Nonnull String commandLabel, @Nonnull CommandInfo info, @Nonnull Method method, @Nonnull Object object) {
         super.registerCommand(commandLabel, info, method, object);
-        
+
         //TODO register command
     }
     
