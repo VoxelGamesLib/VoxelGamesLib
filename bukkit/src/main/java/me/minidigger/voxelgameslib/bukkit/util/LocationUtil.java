a0aa473c9bae9be09004297b872541d359f66373
MiniDigger <admin@minidigger.me>
2017-01-07 20:33:31 +0100

    minor fixes to respawning...

--- bukkit/src/main/java/me/minidigger/voxelgameslib/bukkit/util/LocationUtil.java
+++ bukkit/src/main/java/me/minidigger/voxelgameslib/bukkit/util/LocationUtil.java
@@ -21,11 +21,11 @@ public class LocationUtil {
     }
     
     /**
-     * Convers a vector (+world) into a location
+     * Converts a vector (+world) into a location
      *
      * @param world the location the world should have
      * @param loc   the vector to convert
-     * @return the coverted location
+     * @return the converted location
      */
     public static Location toLocation(String world, Vector3D loc) {
         return new Location(Bukkit.getWorld(world), loc.getX(), loc.getY(), loc.getZ());
